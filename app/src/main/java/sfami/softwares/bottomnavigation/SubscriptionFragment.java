ae674146165ff6571a102b5c97cc7bfe7757ff0c
Sfami <tomufamadi@gmail.com>
2022-12-20 17:56:08 +0200

    Bottom Navigation Template Created.

---
+++ app/src/main/java/sfami/softwares/bottomnavigation/SubscriptionFragment.java
@@ -0,0 +1,22 @@
+package sfami.softwares.bottomnavigation;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SubscriptionFragment extends Fragment {
+
+    public SubscriptionFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_subscriptions, container, false);
+    }
+}
